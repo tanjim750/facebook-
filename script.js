80f8362c71448193d6e307e319e3e606a9ffa26f
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2022-02-19 01:18:05 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const form = document.querySelector("form"),
-statusTxt = form.querySelector(".icon span");
+statusTxt = form.querySelector(".button-area span");
 
 form.onsubmit = (e)=>{
   e.preventDefault();
